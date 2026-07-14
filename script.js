2e36d819c01eb0e7685477947caebf6cd50d2b7c
Shivam Nath Goswami <goswamiji2006@gmail.com>
2026-07-14 21:16:42 +0530

    Added leave meeting functionality

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ console.log(participants);
 const chairs = document.querySelectorAll(".chair");
 console.log(chairs);
 
-chairs.forEach((chair , index) => {
+chairs.forEach((chair, index) => {
     const participant = participants[index];
 
     const avatar = chair.querySelector(".avatar");
@@ -20,22 +20,32 @@ chairs.forEach((chair , index) => {
     avatar.style.backgroundColor = participant.color;
     nameLabel.textContent = participant.name;
 
-    chair.addEventListener("click" , () =>{
+    chair.addEventListener("click", () => {
         chair.classList.toggle("active");
     })
 
     const muteBtn = chair.querySelector(".mute-btn");
 
-    muteBtn.addEventListener("click" , () => {
+    muteBtn.addEventListener("click", () => {
         event.stopPropagation();
 
         const isMuted = muteBtn.textContent === "🎤";
 
-        if(isMuted){
+        if (isMuted) {
             muteBtn.textContent = "🔇";
-        } else{
+        } else {
             muteBtn.textContent = "🎤";
         }
     })
+
+
 })
+
+
+const leavebtn = document.querySelector(".leave-btn");
+
+leavebtn.addEventListener("click", () => {
+    document.querySelector(".container").style.display = "none";
+    document.body.innerHTML += "<h1 style='color: white; font-family: sans-serif;'>You left the meeting</h1>";
+});
 
