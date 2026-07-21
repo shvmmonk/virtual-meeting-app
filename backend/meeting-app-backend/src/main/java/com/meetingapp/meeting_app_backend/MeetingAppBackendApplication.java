75da3f6d66a8cc438cbbccc5e7d46a7c431a3539
Shivam Nath Goswami <goswamiji2006@gmail.com>
2026-07-21 20:08:11 +0530

    Set up initial Spring Boot backend project

---
+++ backend/meeting-app-backend/src/main/java/com/meetingapp/meeting_app_backend/MeetingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.meetingapp.meeting_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/meeting-app-backend/src/test/java/com/meetingapp/meeting_app_backend/MeetingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetingapp.meeting_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
